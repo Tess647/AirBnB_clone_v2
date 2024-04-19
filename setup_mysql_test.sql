ea80a3138b05d8bbf7614269a2ac367139b3fcd7
Tess647 <thanni_taslihah@yahoo.com>
2024-04-19 19:34:17 +0100

    update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
-/* creates a database and a new user with specified permissions */
+--creates a database, new user with specified permissions
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
