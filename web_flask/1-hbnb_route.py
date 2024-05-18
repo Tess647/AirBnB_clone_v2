1a82a258d9cca617767a23d4ae28a1437a1552f0
Tess647 <thanni_taslihah@yahoo.com>
2024-05-18 09:29:08 -0700

    task 0 - 4

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Starts Flask web app
+Listening on 0.0.0.0:5000
+Route:
+	/ - displays "Hello HBNB!"
+	/hbnb - display "HBNB"
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hbnb_route():
+	"""prints Hello HBNB"""
+	return "Hello HBNB!"
+
+ @app.route('/hbnb', strict_slashes=False)
+ def hbnb():
+	"""prints HBNB"""
+	return "HBNB"
+
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
